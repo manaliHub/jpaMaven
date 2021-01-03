36c34afb8ea5d1fb585ca2fc4c5a47c89f92dbd7
prana <prana@10.0.0.45>
2021-01-03 13:44:49 +0530

    added native and hql select query

--- DemoJpaMaven/src/main/java/com/EntityManagerClass.java
+++ DemoJpaMaven/src/main/java/com/EntityManagerClass.java
@@ -1,8 +1,11 @@
 package com;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 public class EntityManagerClass {
 
@@ -60,18 +63,41 @@ public class EntityManagerClass {
     	return stId;
     }
     
+    public static void createNativeQuery() {
+    	EntityManager entityMgr = getEntityManager();
+    	entityMgr.getTransaction().begin();
+    	Query qry = entityMgr.createNativeQuery("select * from student");
+    	List<Student> stList = qry.getResultList();
+    	System.out.println("native query result :"+stList.size());
+    	entityMgr.getTransaction().commit();
+    	
+    }
+    
+    
+    public static void createQuery(int sId) {
+    	EntityManager entityMgr = getEntityManager();
+    	entityMgr.getTransaction().begin();
+    	Query qry = entityMgr.createQuery("select name from Student where sId="+sId);
+    	String name = qry.getSingleResult().toString();
+    	System.out.println("HQ select query result :"+name);
+    	entityMgr.getTransaction().commit();
+    	
+    }
+    
     
 	public static void main(String args[]) {
 		
         Student stObj = new Student();
         stObj.setsId(1);
-        stObj.setName("Manali");
+        stObj.setName("manali");
         stObj.setPhn("123");
         
-        persistData(stObj);
-        findData(1);
-        updateData(1);
-        removeData(1);
+      // persistData(stObj);
+       // findData(1);
+      //  updateData(1);
+       // removeData(1);
+       // createNativeQuery();
+        createQuery(1);
         
         		
 	}
